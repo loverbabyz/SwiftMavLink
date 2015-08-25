ad217b31acbc950f3145d2d84c43c55d1c2e058c
Jonathan Wight <jwight@mac.com>
2015-08-25 12:58:54 -0700

    Scanner improvements.

--- MavlinkTest/Mavlink/MavlinkDataScanner.swift
+++ MavlinkTest/Mavlink/MavlinkDataScanner.swift
@@ -8,10 +8,30 @@
 
 import SwiftUtilities
 
-public class MavlinkDataScanner: DataScanner {
-    public func scan() throws -> Message? {
+public extension DataScanner {
+    func scan() throws -> Message? {
         let message = try Message(buffer: remaining, skipCRC:true)
         current += message.length
         return message
     }
+}
+
+public extension Message {
+
+    static func messagesFromBuffer(buffer:Buffer <Void>) throws -> [Message] {
+        var messages:[Message] = []
+        let scanner = DataScanner(buffer: buffer.bufferPointer.toUnsafeBufferPointer())
+        while scanner.atEnd == false {
+            if let _ = try scanner.scanUpTo(0xFE) {
+        //        print("Skipping: \(junk.asHex)")
+            }
+            guard let message:Message = try scanner.scan() else {
+                break
+            }
+            messages.append(message)
+        }
+        return messages
+    }
+
+
 }
